9c38adeeeaf5098b94ba2ac1935e09222e1e5a01
LeeSeokBong <kingstar6035@gmail.com>
2022-01-02 22:21:46 +0900

    할 일 정리 및 페이지 추가

---
+++ index.js
@@ -0,0 +1,4 @@
+const startBtn = document.querySelector("#startBtn")
+startBtn.addEventListener("click", function(){
+    location.href="page/previousLife.html"
+})
---
+++ page/previousLife.js
@@ -0,0 +1,4 @@
+const startBtn = document.querySelector(".homeBtn")
+startBtn.addEventListener("click", function(){
+    location.href="../index.html"
+})
