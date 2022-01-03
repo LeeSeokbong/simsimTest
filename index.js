3cb365893cc962af52a0013cd3c8cf2ece3f57b2
LeeSeokBong <kingstar6035@gmail.com>
2022-01-03 23:10:49 +0900

    썸네일 그림 추가 및 크기 정리

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const startBtn = document.querySelector("#startBtn")
+const startBtn = document.querySelector("#previousLifeTest .homeTestStartBtn")
 startBtn.addEventListener("click", function(){
     location.href="page/previousLife.html"
 })
